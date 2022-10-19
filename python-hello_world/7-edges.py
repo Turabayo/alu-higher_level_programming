83a402ef3525959a43dc578c25b31fa0aa30bd79
Turabayo <i.turabayo@alustudent.com>
2022-10-19 06:47:28 -0700

    cmd for printing slices

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,1 +1,8 @@
-`
+#!/usr/bin/python3
+word="Holberton"
+First_3_letters = word[:3]
+Last_2_letters = word[-2:]
+Middle_word = word[1:-1]
+print(f"First_3_letters: {First_3_letters}")
+print(f"Last_2_letters: {Last_2_letters}")
+print(f"Middle word: {Middle_word}")
