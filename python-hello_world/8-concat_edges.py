7fc371d754a3d72b604069877a1c87653471402a
Turabayo <i.turabayo@alustudent.com>
2022-10-19 14:24:41 -0700

    cmd for printing a sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print('object oriented programming with python')
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+print(str)
