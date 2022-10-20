83a8a361f2d6a62cb43799770e93fd5673ff5061
Turabayo <i.turabayo@alustudent.com>
2022-10-20 03:16:11 -0700

    cmd for printing a sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, inteactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
-str1 = str[39:66]+str[107:113]+str[0:6]
+str1 = str[38:65]+str[106:112]+str[0:6]
 print(str1)
