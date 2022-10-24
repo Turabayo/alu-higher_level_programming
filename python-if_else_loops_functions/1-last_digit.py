57d5f2032dea585bbb23ac3f1615f56b44129d04
Turabayo <i.turabayo@alustudent.com>
2022-10-24 04:40:11 -0700

    command for printing the last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 if number > 5:
-    print(f' the last digit {number}:the string is greater than 5')
+    print(f'the last digit of {number} the string is greater than 5')
 elif number == 0:
-    print(f'the last digit {number}: the string and is 0')
+    print(f'the last digit of {number} the string and is 0')
 elif number < 0:
-    print(f' the digit {number}: the string is less than 6 and not 0')   
+    print(f'the last digit of {number} the string is less than 6 and not 0')  
