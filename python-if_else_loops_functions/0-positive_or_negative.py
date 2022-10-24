321bce11661b892f76955c6c0e76e96db1ab8e1f
Turabayo <i.turabayo@alustudent.com>
2022-10-24 03:41:57 -0700

    command for printing if number is positive or neagative

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,12 +2,11 @@
 import random
 number = random.randint(-10, 10)
 number = int(input("Please Enter the number:"))
-please enter the number: 98
 if number < 0:
-    print('the number is negative')
+    print(f'{number} is negative')
 elif number > 0:
-    print('the number is positive')
+    print(f'{number} is positive')
 elif number == 0:
-    print('the number is zero')
+    print(f'{number} is zero')
 else:
     print('Nothing')
