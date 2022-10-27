4655957fa9c7ff7c23e1b04aa2387908436aa585
Turabayo <i.turabayo@alustudent.com>
2022-10-27 02:41:03 -0700

    updated

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    j = 0
+    k = len(argv)
+    for i in range(1, k):
+        j += int(argv[i])
+    print(f"{j}")
---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    j = list(dir(hidden_4))
+    for i in j:
+        if i[1] == "_":
+            continue
+        else:
+            print(f"{i}")
