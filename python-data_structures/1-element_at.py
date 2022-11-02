41ca33fd161c4a01e807edb6c9f3e74953812a8e
Turabayo <i.turabayo@alustudent.com>
2022-11-02 08:36:21 -0700

    function that retrieves an element from a list

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    idx = int(input("please enter an integer: "))
+    if idx > 0:
+        print("negative number")
+        if idx < 0:
+            print("positive number")
