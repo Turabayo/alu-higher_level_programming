ab132f67a37f944c8917f88c0f5cd588aa5aa735
Turabayo <i.turabayo@alustudent.com>
2022-11-02 09:09:41 -0700

    function that prints element in integer

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    idx = int(input("please enter an integer: "))
-    if idx > 0:
-        print("negative number")
-        if idx < 0:
-            print("positive number")
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    else:
+        return my_list[idx]
+
+
+a = [1, 2, 3, 4, 5]
+print(element_at(a, 44444))
