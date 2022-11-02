674833d250d300c1f2295b54763cf754261b18e4
Turabayo <i.turabayo@alustudent.com>
2022-11-02 10:55:32 -0700

    replacing an element of list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,8 +4,8 @@
 def replace_in_list(my_list, idx, element):
     new_list = []
     if idx < 0 or idx > (len(my_list) - 1):
-        return my-list
+        return my_list
     else:
         my_list[idx] = element
-        new-list = my_list
+        new_list = my_list
         return new_list
