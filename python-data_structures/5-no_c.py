eb2aa565a7ecf1aba0cc2b21d73ccfd10828095c
Turabayo <i.turabayo@alustudent.com>
2022-11-03 05:56:49 -0700

    A function that removes all characters

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    {ord('c'): None, ord('C'): None}
+    for i in my_string:
+        if ord('i') == 99 or 67:
+            my_string.translate({ord('c'): None, ord('C'): None})
+        return my_string.translate({ord('c'): None, ord('C'): None})
