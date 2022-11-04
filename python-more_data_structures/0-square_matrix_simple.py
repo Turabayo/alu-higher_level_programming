9f00e3e0ca0d8fe5019e26ff75f0f9cb8f84cdcd
Turabayo <i.turabayo@alustudent.com>
2022-11-04 13:52:52 -0700

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@
 
 def square_matrix_simple(matrix=[]):
     if not matrix:
-       return None
+        return None
     return[[num * num for num in row] for row in matrix]
--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    if not my_list:
+    if  not my_list:
         return new_list
     for i in range(len(my_list)):
         if new_list[i] == search:
