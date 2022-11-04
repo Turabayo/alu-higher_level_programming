0cff89d88b7c7acd16acd201f2702340da74b276
Turabayo <i.turabayo@alustudent.com>
2022-11-04 08:44:30 -0700

    task0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 
-def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    if not my_list:
-       return new_list
-    for i in range(len(my_list)):
-        if new_list[i] == search:
-           new_list[i] = replace
-    return new_list
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+       return None
+    return[[num * num for num in row] for row in matrix]
--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 
+def search_replace(my_list, search, replace):
+
+        new_list = my_list.copy()
+
+            if not my_list:
+
+                        return new_list
+
+                    for i in range(len(my_list)):
+
+                        if new_list[i] == search:
+
+                                        new_list[i] = replace
 
 
 
-
-def uniq_add(my_list=[]):
-
-        result = 0
-
-            for x in set(my_list):
-
-                        result += x
-
-                            return (result)))])
+                                            return new_list]]))))
