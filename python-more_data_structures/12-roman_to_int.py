c0e9791963af677abf9d9701e28af3c6e0d68a15
Turabayo <i.turabayo@alustudent.com>
2022-11-04 12:29:13 -0700

    task12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -8,8 +8,8 @@ def roman_to_int(roman_string):
     converter = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     for current, next in zip(roman_string, roman_string[1:]):
         if converter[current] >= converter[next]:
-           int_sum += converter[current]
+            int_sum += converter[current]
         else:
-           int_sum -= converter[current]
+            int_sum -= converter[current]
     int_sum += converter[roman_string[-1]]
     return int_sum
