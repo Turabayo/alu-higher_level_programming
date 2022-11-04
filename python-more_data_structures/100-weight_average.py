4a0159b7f1cbff28e8ec5cc86a7f33815fdd99f6
Turabayo <i.turabayo@alustudent.com>
2022-11-04 13:11:26 -0700

    Weighted average

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -7,6 +7,6 @@ def weight_average(my_list=[]):
     avg = 0
     size = 0
     for tup in my_list:
-          avg += (tup[0] * tup[1])
-          size += tup[1]
+           avg += (tup[0] * tup[1])
+           size += tup[1]
     return (avg / size)
