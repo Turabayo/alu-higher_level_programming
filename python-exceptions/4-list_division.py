cd74a309b16a9a4b23924b59aefd6e77d268e481
Turabayo <i.turabayo@alustudent.com>
2022-11-11 03:05:35 -0800

    Divide a list please Enter for your changes. Lines starting

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
-           new_list.append(my_list_1[i] / my_list_2[i])
+            new_list.append(my_list_1[i] / my_list_2[i])
         except TypeError:
             print("wrong type")
             new_list.append(0)
@@ -13,8 +13,8 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             new_list.append(0)
         except IndexError:
-           print("out of range") 
-           new_list.append(0)
+            print("out of range") 
+            new_list.append(0)
         finally:
            pass
     return new_list   
