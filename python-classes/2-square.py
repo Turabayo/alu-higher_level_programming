4ab98fbbe0e27e9e7f25cb03ac7e705f4fc23ae7
Turabayo <i.turabayo@alustudent.com>
2022-11-11 06:17:08 -0800

    Size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# class Square that defines a square by: (based on 1-square.py)
+"""
+   define a class 'Square'
+"""
+
+
+class Square:
+    """ 
+        square with private instance attribute: 'size'
+    """
+
+def __init__(self, size=0):
+        """
+             Args:
+                  size (int): size of the new square
+          """
