6bfeff1c7fb8f9907565addbe6ea5ea6f9d6e452
Turabayo <i.turabayo@alustudent.com>
2022-11-11 07:12:13 -0800

    rea of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# class 'Square' that defines a square by:
+# (based on 2-square.py)
+"""
+   define a class 'Square'
+"""
+
+
+class Square:
+    """
+      square with private instance attribute: 'size'
+    """
+
+    def __inint__(self, size=0):
+        """
+            Args:
+                size (int):size of thr new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        self.___size =size 
+    def area(self):
+        """
+            Return:area of the square
+        """
+        return(self.__size * self.__size)
