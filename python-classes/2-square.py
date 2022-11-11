6990dc3b31696b2d769d5ebe0db955796397246a
Turabayo <i.turabayo@alustudent.com>
2022-11-11 06:21:18 -0800

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,12 +6,13 @@
 
 
 class Square:
-    """ 
-        square with private instance attribute: 'size'
-    """
+ """ 
+   square with private instance attribute: 'size'
+"""
+
 
 def __init__(self, size=0):
         """
-             Args:
-                  size (int): size of the new square
-          """
+          Args:
+              size (int): size of the new square
+        """
