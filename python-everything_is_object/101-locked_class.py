908b70bbbfb378c66239c2137e973cfe1ea1f0b3
Turabayo <i.turabayo@alustudent.com>
2022-11-16 05:05:53 -0800

    Low memory cost

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -4,8 +4,8 @@
 
 class LockedClass():
    """ Prevents the user from dynamically creating new instance attributes
-    except if the new instance attribut is called first_name
-    :param None:
+   except if the new instance attribut is called first_name
+   :param None:
    """
 
-__slots__ = ["first_name"]
+   __slots__ = ["first_name"]
