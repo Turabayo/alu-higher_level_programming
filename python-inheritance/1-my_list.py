e00cbfde588a5976e19acad7a3aa3c6b7ffbe060
Turabayo <i.turabayo@alustudent.com>
2022-11-17 03:53:28 -0800

    My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+
+class MyList(list):
+    """Inheritance form superclass"""
+    def __init__(self):
+        """initialisation of object"""
+        super().__init__()
+        def print_sorted(self):
+         """prints sorted list"""
+         print(sorted(self))
