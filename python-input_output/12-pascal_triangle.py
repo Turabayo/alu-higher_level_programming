9896999d200451c2c749d4ca676ae3e2532e5faa
Turabayo <i.turabayo@alustudent.com>
2022-11-18 14:11:39 -0800

    Pascal's Triangle'

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,21 +3,21 @@
 
 
 def pascal_triangle(n):
-     """represents pascal's triangle"""
-     if n <= 0:
-         return []
-     answer = []
-     for k in range(n):
-         if k == 0:
-              answer.append([1])
-         else:
-             ans = []
-             list = answer[k - 1][:]
-             list.insert(0, 0)
-             list.append(0)
-             i = 0
-             while i < len(list) - 1:
-                    ans.append(list[i] + list[i + 1])
-                    i = i + 1
-             answer.append(ans)
-     return answer
+    """represents pascal's triangle"""
+    if n <= 0:
+        return []
+    answer = []
+    for k in range(n):
+        if k == 0:
+            answer.append([1])
+        else:
+            ans = []
+            list = answer[k - 1][:]
+            list.insert(0, 0)
+            list.append(0)
+            i = 0
+            while i < len(list) - 1:
+                   ans.append(list[i] + list[i + 1])
+                   i = i + 1
+            answer.append(ans)
+    return answer
