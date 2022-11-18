b9d6e021f20733e733c0fa8418c20480a48bd00a
Turabayo <i.turabayo@alustudent.com>
2022-11-18 12:52:49 -0800

    Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" read file function"""
+
+def read_file(filename=""):
+    """reads a file and prints it out"""
+    with open(filename, encoding="utf-8") as f:
+         print(f.read(), end="")
