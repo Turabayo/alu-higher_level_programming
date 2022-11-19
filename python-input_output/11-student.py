f46015413f080525ebfdc9e2807e51adcd5a6152
Turabayo <i.turabayo@alustudent.com>
2022-11-19 12:01:14 -0800

    Student to disk and reload

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -5,6 +5,7 @@
 class Student:
     """Represent a student."""
 
+
 def __init__(self, first_name, last_name, age):
     """Initialize a new Student.
 
@@ -16,6 +17,7 @@ Args:
     self.first_name = first_name
     self.last_name = last_name
     self.age = age
+
 
 def to_json(self, attrs=None):
     """ Get a dictionary representation of the Student.
