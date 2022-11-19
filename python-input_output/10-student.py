603413065988a927b87786e208a2f02e73a1704c
Turabayo <i.turabayo@alustudent.com>
2022-11-19 11:55:37 -0800

    Student to disk and reload

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
-"""define class Student"""
+"""Defines a class Student."""
 
 
 class Student:
-    """define student instance attributes"""
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+    """Represent a student."""
 
-        def to_json(self, attributes=None):
-            """retrieves dictionary representation"""
-            json = dict(vars(self))
-            if type(attributes) == list:
-                for j in list(json):
-                    if j not in attributes:
-                        json.pop(j, None)
-            return json
-            return vars(self)
+    def __init__(self, first_name, last_name, age):
+
+                """Initialize a new Student.
+
+                Args:
+                    first_name (str): The first name of the student.
+                    last_name (str): The last name of the student.
+                    age (int): The age of the student.
+                """
+                self.first_name = first_name
+                self.last_name = last_name
+                self.age = age
+
+    def to_json(self, attrs=None):
+            """Get a dictionary representation of the Student.
+
+             If attrs is a list of strings, represents only those attributes
+             included in the list.
+
+             Args:
+                 attrs (list): (Optional) The attributes to represent.
+             """
+        if (type(attrs) == list and
+               all(type(ele) == str for ele in attrs)):
+           return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+       return self.__dict__
--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-"""define class Student"""
+"""Defines a class Student."""
 
 
 class Student:
-     """define student instance attributes"""
-     def __init__(self, first_name, last_name, age):
-         self.first_name = first_name
-         self.last_name = last_name
-         self.age = age
+    """Represent a student."""
 
-     def to_json(self, attributes=None):
-         """retrieves dictionary representation"""
-         json_l = dict(vars(self))
-         if type(attributes) == list:
-             for j in list(json_l):
-                 if j not in attributes:
-                     json.pop(j, None)
-                     return json_l
+def __init__(self, first_name, last_name, age):
+    """Initialize a new Student.
 
-         def reload_from_json(self, json):
-             """replaces all attributes of the Student instance"""
-             for i in list(json):
-                 setattr(self, i, json[i])
+Args:
+    first_name (str): The first name of the student.
+    last_name (str): The last name of the student.
+    age (int): The age of the student.
+   """
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+def to_json(self, attrs=None):
+    """ Get a dictionary representation of the Student.
+
+    If attars is a list of strings, represents only those attributes
+    included in the list.
+
+    Args:
+        attrs (list): (Optional) The attributes to represent
+     """
+    if (type(attrs) == list and
+            all(type(ele) == str for ele in attrs)):
+         return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+    return self.__dict__
