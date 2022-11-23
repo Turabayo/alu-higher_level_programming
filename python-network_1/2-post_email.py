ced809cd6019d438c175dcb1ca62707062b3bcd8
turabayo <i.turabayo@alustudent.com>
2022-11-23 05:40:45 -0800

    POST an email #0

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,11 +3,17 @@
 """
 
 import urllib.request
-from sys import argv
+import urllib.parse
+import sys
 
 
-if __name__ == "__main__":
-    req = urllib.request.Request(argv[1])
-    with urllib.request.urlopen(req) as response:
-        data = response.getheader('X-Request-Id')
-        print(data)
+if __name__ == '_main__':
+
+    url = sys.argv[1]
+    message = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(message)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as reply:
+        content = reply.read()
+        print("{}".format(content.decode("utf-8")))
