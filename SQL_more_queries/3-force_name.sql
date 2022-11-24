bf48ad08187964f3dbe174f6cd9637de3a7aa834
turabayo <i.turabayo@alustudent.com>
2022-11-24 07:45:42 -0800

    Always a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 --a script that creates the table
-CREATE TABLE IF NOT EXISTS force_name (
-	id INT,
-	name VARCHAR(255)
-	);
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
+);
