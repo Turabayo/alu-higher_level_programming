05778e7fc2c4a87e8afabcca2356e54bd4c0f390
turabayo <i.turabayo@alustudent.com>
2022-11-24 09:14:36 -0800

    Best genre

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,4 @@
---Best genre
+-- script that lists all genres in the database 'hbtn_0d_tvshows_rate' by their rating
 SELECT `name`, SUM(`rate`) AS `rating`
   FROM `tv_genres` AS g
        INNER JOIN `tv_show_genres` AS s
