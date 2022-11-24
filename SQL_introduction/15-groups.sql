71ccc9d110569209b71b1b0903fc4f651013234a
turabayo <i.turabayo@alustudent.com>
2022-11-24 03:42:09 -0800

    Number by score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- no link
-SELECT score, name 
-FROM second_table
-WHERE name IS NOT NULL AND name != ''
-ORDER BY score DESC;
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
