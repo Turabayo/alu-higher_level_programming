ccaf490f486403feaacb97727c31f750b603c1ca
turabayo <i.turabayo@alustudent.com>
2022-11-24 06:41:19 -0800

    Uniqueid

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 --Unique ID
-CREATE TABLE IF NOT EXISTS `unique_id` (
-    `id`   INT          DEFAULT 1 UNIQUE,
-    `name` VARCHAR(256)
+CREATE TABLE IF NOT EXISTS unique_id (
+     id   INT          DEFAULT 1 UNIQUE,
+     name VARCHAR(256)
 );
