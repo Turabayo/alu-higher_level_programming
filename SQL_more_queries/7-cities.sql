c8b14af8ff86ebe37ff8f809699a3a1e4424c0d7
turabayo <i.turabayo@alustudent.com>
2022-11-24 08:02:23 -0800

    Cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
---cities table
+--creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
     PRIMARY KEY(`id`),
