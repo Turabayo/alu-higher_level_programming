a44e26f8a455aa9b1fa4c3c0839615eec893644f
turabayo <i.turabayo@alustudent.com>
2022-11-24 08:11:00 -0800

    Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
---a script that lists all cities contained in the database hbtn_0d_usa
+--lists all cities contained in the database hbtn_0d_usa
 SELECT c.`id`, c.`name`, s.`name`
   FROM `cities` AS c
        INNER JOIN `states` AS s
