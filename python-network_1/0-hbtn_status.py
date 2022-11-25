4784a12e993e1c284a83384dbd4d9058a507d45c
turabayo <i.turabayo@alustudent.com>
2022-11-25 03:42:38 -0800

    status0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,6 @@
 
 #!/usr/bin/python3
-"""A script that
-- fetches https://intranet.hbtn.io/status.
-- uses urlib package
-"""
-
+#A script that fetches https://intranet.hbtn.io/status and uses urlib package
 
 if __name__ == '__main__':
     import urllib.request
