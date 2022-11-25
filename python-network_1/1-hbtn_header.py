65e8ff934bc6cfbc4452cec4463e877b95febfd1
turabayo <i.turabayo@alustudent.com>
2022-11-25 03:40:54 -0800

    Response header value

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
-#scripts that takes URL,Sends a request to the URL and displays the value of the X-request
+ #!/usr/bin/python3
+#script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id found in header
 
 import urllib.request
 import sys
 
-if__name__= "__main__":
-    with urllib.request.urlopen(sys.argv[1] as res:
-            print(res.headers.get('X-request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        print(res.headers.get('X-Request-Id'))
