858cbbf6dda24da72b6c256723bb138e5a2812ef
Turabayo <i.turabayo@alustudent.com>
2022-12-09 10:44:59 +0000

    this function adds two integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 # a function that adds 2 integers
 """
-	define function 'add_integer'
+    define function 'add_integer'
 """
 
 
 def add_integer(a, b=98):
     """
-	this function return s the sum of two integers
+        this function return s the sum of two integers
     """
     if type(a) != float and type(a) != int:
         raise TypeError('a must be an integer')
