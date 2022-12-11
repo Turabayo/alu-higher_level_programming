8c85e32c23d18a8999ff022452e7927117ed53ed
Turabayo <i.turabayo@alustudent.com>
2022-12-11 06:16:58 -0800

    Print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,14 +3,14 @@
 
 
 def print_square(size):
-	"""function that prints a square"""
+    """function that prints a square"""
     if type(size) !=int:
-	raise TypeError("size must be an integer")
+        raise TypeError("size must be an integer")
     if type(size) < 0:
-	raise TypeError("size must be >= 0")
+        raise TypeError("size must be >= 0")
     if size == 0:
-	return
-   else:
-	for x in range(size)
-	    print("#" * size)		
+        return
+    else:
+        for x in range(size):
+            print("#" * size)		
 
