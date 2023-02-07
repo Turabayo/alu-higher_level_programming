a46d0df8649c4875591c693d43b12f47057572a9
Turabayo <i.turabayo@alustudent.com>
2023-02-07 04:12:40 -0800

    Javascript

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const firstArgv = parseInt(process.argv[2]);
 if (isNaN(firstArgv)) {
-	  console.log('Not a number');
+  console.log('Not a number');
 } else {
-	  console.log('My number: ' + firstArgv);
+  console.log('My number: ' + firstArgv);
 }
