83d96f63e2daa675cb99214055b27d31a7eb0da6
Turabayo <i.turabayo@alustudent.com>
2023-02-07 04:53:03 -0800

    Javascript

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const firstArgv = process.argv[2];
 if (isNaN(firstArgv)) {
-	  console.log('Missing size');
+  console.log('Missing size');
 } else {
-	  for (let i = 0; i < firstArgv; i++) {
-		      console.log('X'.repeat(firstArgv));
-		    }
+  for (let i = 0; i < firstArgv; i++) {
+   console.log('X'.repeat(firstArgv));
+  }
 }
