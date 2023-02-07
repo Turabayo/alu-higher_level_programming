45b821c8023cf504394b8a44dabe28e65d267e3f
Turabayo <i.turabayo@alustudent.com>
2023-02-07 04:44:00 -0800

    Javascript

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const firstArgv = process.argv[2];
 if (isNaN(firstArgv)) {
-	  console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	  for (let i = 0; i < firstArgv; i++) {
-		      console.log('C is fun');
-		    }
+  for (let i = 0; i < firstArgv; i++) {
+    console.log('C is fun');
+    }
 }
