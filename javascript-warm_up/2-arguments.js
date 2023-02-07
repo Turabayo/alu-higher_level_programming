e605c0d3b5efe7fe5a397886146effa38d3bc350
Turabayo <i.turabayo@alustudent.com>
2023-02-07 04:03:39 -0800

    Javascript

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
 switch (process.argv.length) {
-<<<<<<< HEAD
-  case 2:
-   console.log('No argument');
-   break;
-  case 3:
-   console.log('Argument found');
-   break;
-  default:
-   console.log('Arguments found');
-   break;
-}
-=======
+
 	case 2:
 	   console.log('No argument');
 	   break;
@@ -22,4 +11,3 @@ switch (process.argv.length) {
 	    console.log('Arguments found');
 	    break;
 }
->>>>>>> 220ed8a3af3387c164f52dc12cd2b50f723a64f0
