b707fcc70109ee5b928cc54e65e0c52adf1b9a26
Turabayo <i.turabayo@alustudent.com>
2023-02-08 10:11:47 +0000

    Object,scopes and closures

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,15 +2,15 @@
 const myArgs = process.argv.length;
 const myArray = [];
 switch (myArgs) {
-	  case 2:
-		  case 3:
-		    console.log(0);
-		    break;
-		  default:
-		    for (let i = 2; i < myArgs; i++) {
-			          myArray.push(process.argv[i]);
-			        }
-		    myArray.sort(function (a, b) { return b - a; });
-		    console.log(myArray[1]);
-		    break;
+  case 2:
+  case 3:
+    console.log(0);
+    break;
+  default:
+    for (let i = 2; i < myArgs; i++) {
+	myArray.push(process.argv[i]);
+   }
+    myArray.sort(function (a, b) { return b - a; });
+    console.log(myArray[1]);
+    break;
 }
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 const a = process.argv[2];
 const b = process.argv[3];
 function add (a, b) {
-	  return (parseInt(a) + parseInt(b));
+ return (parseInt(a) + parseInt(b));
 }
 
 console.log(add(a, b));
